736e8aa13d958efedb915d80e077b575ecaeac3d
Aleksandr Kuzmin <a-kuzmin.dev@yandex.ru>
2025-03-13 21:51:09 +0300

    feat: Добавил обработчики удаления, изменения сотрудника и получении о нем информации по ID

--- controllers/employees.js
+++ controllers/employees.js
@@ -22,11 +22,11 @@ const createEmployee = async (req, res) => {
             });
         }
 
-        await prisma.employee.create({
+        const employee = await prisma.employee.create({
             data: { firstName, lastName, age, address, userId },
         });
 
-        return res.status(201).json({ message: 'Сотрудник создан' });
+        return res.status(201).json(employee);
     } catch (err) {
         return res.status(500).json({
             message: 'Произошла ошибка при создании сотрудника',
@@ -35,15 +35,110 @@ const createEmployee = async (req, res) => {
 };
 
 const getEmployeeById = async (req, res) => {
-    res.status(200).json({ message: `get employees id ${req.params.id}` });
+    const id = req.params.id;
+
+    try {
+        const employee = await prisma.employee.findFirst({
+            where: {
+                id,
+            },
+        });
+
+        if (!employee) {
+            return res.status(404).json({ message: 'Сотрудник не найден' });
+        }
+
+        return res.status(200).json(employee);
+    } catch {
+        return res.status(500).json({
+            message:
+                'Не удалось получить информацию о сотруднике, попробуйте позже.',
+        });
+    }
 };
 
 const editEmployeeById = async (req, res) => {
-    res.status(200).json({ message: `change employee id ${req.params.id}` });
+    try {
+        const { firstName, lastName, age, address } = req.body;
+        const id = req.params.id;
+        const { id: userId } = req.user;
+
+        if (!firstName || !lastName || !age || !address) {
+            return res.status(400).json({
+                message: 'Пожалуйста, заполните обязательные поля',
+            });
+        }
+
+        const employee = await prisma.employee.findFirst({
+            where: {
+                id,
+            },
+        });
+
+        if (!employee) {
+            return res
+                .status(404)
+                .json({ message: 'Не удалось найти сотрудника.' });
+        }
+
+        if (userId !== employee.userId) {
+            return res.status(403).json({
+                message: 'Недостаточно прав для выполнения операции',
+            });
+        }
+
+        const updatedEmployee = await prisma.employee.update({
+            where: {
+                id,
+            },
+            data: { firstName, lastName, age, address, userId },
+        });
+
+        return res.status(200).json(updatedEmployee);
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Не удалось отредактировать сотрудника',
+        });
+    }
 };
 
 const deleteEmployeeById = async (req, res) => {
-    res.status(200).json({ message: `delete employee id ${req.params.id}` });
+    try {
+        const id = req.params.id;
+        const userId = req.user.id;
+
+        const employee = await prisma.employee.findFirst({
+            where: {
+                id,
+            },
+        });
+
+        if (!employee) {
+            return res
+                .status(404)
+                .json({ message: 'Не удалось найти сотрудника.' });
+        }
+
+        if (userId !== employee.userId) {
+            return res.status(403).json({
+                message: 'Недостаточно прав для выполнения операции',
+            });
+        }
+
+        await prisma.employee.delete({
+            where: {
+                id,
+            },
+        });
+
+        res.status(200).json({
+            message: `delete employee id ${req.params.id}`,
+        });
+    } catch {
+        return res.status(500).json({
+            message: 'Не удалось удалить сотрудника, попробуйте позже.',
+        });
+    }
 };
 
 module.exports = {
